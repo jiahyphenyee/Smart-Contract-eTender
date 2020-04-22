56652975987cb93af7ab775260fefee23509fae0
jiahyphenyee <40422191+jiahyphenyee@users.noreply.github.com>
2020-04-22 14:14:07 +0800

    added contract variables

--- tender.sol
+++ tender.sol
@@ -1,5 +1,30 @@
 pragma solidity ^0.4.24;
 
 contract Tender {
+    
+    struct Bid {
+        bytes32 hiddenBid;
+        uint deposit;
+    }
+    
+    uint public tenderCloseDate;
+    uint public revealEnd;
+    bool public ended;
+    
+    mapping(address => Bid[]) public bids;
+    mapping(address => uint) withdrawnBids;
+    
+    //events
+    event TenderClosed(address winner, uint lowestBid);
+    
+    //modifiers
+    
+    constructor (
+        uint _tenderDuration
+        
+    ) public {
+        tenderCloseDate = now + _tenderDuration;
+    }
+    
 
 }
